ae080860bbf7945a097e5d69f69a7bfbcfc31c9b
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-02-04 21:01:27 +0500

    error middleware added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,20 @@
-const registerUser = (req, res) => {
-  res.send('registerUser');
-};
+const asyncHandler = require('express-async-handler');
 
-const loginUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, emaii, password } = req.body;
+
+  if (!name || !emaii || !password) {
+    return res.status(400).json({
+      message: 'Please provide all the required fields',
+    });
+  }
+
+  res.send('registerUser');
+});
+
+const loginUser = asyncHandler(async (req, res) => {
   res.send('loginUser');
-};
+});
 
 module.exports = {
   registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const colors = require('colors');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 const app = express();
-
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Support Desk API' });
 });
 
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
