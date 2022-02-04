5e509980c5b3bd986e4e761bee738f33959f3687
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-02-04 22:27:36 +0500

    functionality of login register and jwt done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -81,7 +81,17 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
+});
+
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
